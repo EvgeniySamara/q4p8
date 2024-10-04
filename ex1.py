9d23f41371e9c0688f0c2f4f444521b94c566694
KRUGLYANSKIY EVGENIY <EMKRUG63@GMAIL.COM>
2024-10-04 15:01:48 +0400

    ex1 complete

--- ex1.py
+++ ex1.py
@@ -24,3 +24,96 @@
 # pickle.dump() для Pickle.
 
 import os
+import json
+import csv
+import pickle
+
+
+
+def get_size(path):
+    """  Возвращает размер файла или директории.
+    """
+    if os.path.isfile(path): # Если путь - файл, возвращаем его размер
+        return os.path.getsize(path)
+    elif os.path.isdir(path):
+        total_size = 0
+
+# Если путь - директория, рекурсивно вычисляем размер всех файлов в директории
+    for dirpath, _, filenames in os.walk(path):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            total_size += os.path.getsize(file_path)
+    return total_size
+
+
+def traverse_directory(directory):
+    """Рекурсивно обходит директорию и возвращает информацию о файлах
+    и директориях."""
+    result = []
+    # Обход директории с помощью os.walk, который возвращает корневую директорию, поддиректории и файлы
+    for root, dirs, files in os.walk(directory):
+        for name in dirs + files:
+            path = os.path.join(root, name)
+            is_dir = os.path.isdir(path)
+            size = get_size(path)
+            parent = os.path.basename(root)
+            # Добавление информации о текущем объекте в список результата
+            result.append({
+            'name': name,
+            'path': path,
+            'type': 'directory' if is_dir else 'file',
+            'size': size,
+            'parent': parent
+            })
+    return result
+
+def save_to_json(data, file = 'testjson.json'):
+    with open (file,"w") as f:
+        json.dump(data,f)
+
+
+def save_to_csv(data, file = 'testcsv.csv'):
+    with open (file,"w") as f:
+        writer = csv.DictWriter(f,fieldnames=['name', 'path', 'type', 'size', 'parent'])
+        writer.writeheader()
+        writer.writerows(data)
+
+def save_to_pickle(data, file = 'test.pickle'):
+
+    with open(file, 'wb') as pickle_file:
+        pickle.dump(data, pickle_file)
+
+
+
+# def obhod(target_dir):
+#     res = dict()
+#     for root,dirs,files in os.walk(target_dir):
+#         for file in files:
+#             # print(os.path.join(root,file))
+#
+#
+#             # os.path.isfile(os.path.join(root, file))
+#             res[file] = ([root,file,os.path.getsize(os.path.join(root, file))])
+#         for file in files:
+#             # print(os.path.join(root,file))
+#
+#
+#             # os.path.isfile(os.path.join(root, file))
+#             res[file] = ([root,dir,os.path.getsize(os.path.join(root, file))])
+#
+#     return res
+
+
+if __name__ == '__main__':
+    target_dir = r'c:\Users\User\PycharmProjects\Gbpractic\q4p8'
+
+    if not os.path.isdir(target_dir):
+        print('Директория не найдена')
+    else:
+        # res = obhod(target_dir)
+        # print(os.path.basename(f"{target_dir}\\ex1.py"))
+        res = traverse_directory(target_dir)
+        print (*res, sep='\n')
+        save_to_json(res)
+        save_to_csv(res)
+        save_to_pickle(res)
